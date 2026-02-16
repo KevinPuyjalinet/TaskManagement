32c7db1858d703720aa65e9cc580765eb004976e
Kévin Puyjalinet <puyjalinet.kevin@gmail.com>
2026-02-16 14:49:26 +0100

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Kévin PUYJALINET on 16/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
